7678c2c612b9c9b33536e19bcd7a486389f8eb63
ManavSurani <manavsurani982@gmail.com>
2026-05-07 18:09:23 +0530

    refactor: remove redundant utility scripts and temporary development files

--- check_project.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const projects = await prisma.project.findMany({
-    include: { customer: { select: { id: true, customerName: true } }, milestones: true }
-  });
-  console.log(JSON.stringify(projects, null, 2));
-}
-main().finally(() => prisma.$disconnect());
--- create-shortcut.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd "%~dp0"
-echo Setting up PNP CRM Desktop Shortcut...
-powershell.exe -ExecutionPolicy Bypass -File .\create-shortcut.ps1
-echo Setup complete.
-pause
-popd
